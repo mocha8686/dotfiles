a663f609abe8a9a3091e3c3dfae3afeebedd323e
mocha8686 <june.mocha8686@gmail.com>
2024-06-03 21:26:11 -0700

    Add svelte to null-ls

--- nvim/config/null_ls.lua
+++ nvim/config/null_ls.lua
@@ -2,4 +2,4 @@ local mason_null_ls = require 'mason-null-ls'
 local null_ls = require 'null-ls'
 
 mason_null_ls.setup(require 'opts.mason_null_ls')
-null_ls.setup()
+null_ls.setup(require 'opts.null_ls')
---
+++ nvim/opts/null_ls.lua
@@ -0,0 +1,11 @@
+local null_ls = require 'null-ls'
+
+local opts = {
+	sources = {
+		null_ls.builtins.formatting.prettierd.with({
+			extra_filetypes = { 'svelte' },
+		}),
+	},
+}
+
+return opts
