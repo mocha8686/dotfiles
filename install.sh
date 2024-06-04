c6bff91d3d2f380eeb2157809f996982acc91d78
mocha8686 <june.mocha8686@gmail.com>
2024-06-03 21:25:50 -0700

    Create missing directories

--- install.sh
+++ install.sh
@@ -3,8 +3,13 @@ for entry in $(cat config.txt); do
 	filename=$(echo $entry | cut -d ':' -f 1)
 	destdir=$(echo $entry | cut -d ':' -f 2)
 
+	destdirexpanded=${destdir/#\~/$HOME}
 	src=$(pwd)/$filename
-	dest=${destdir/#\~/$HOME}/$filename
+	dest=$destdir/$filename
+
+	if [[ ! -d $destdir ]]; then 
+		mkdir -p $destdir
+	fi
 
 	ln -sni $src $dest
 done
