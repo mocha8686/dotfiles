2e18d5bd6455de89bf114da19bda7d5d87716030
mocha8686 <june.mocha8686@gmail.com>
2024-06-03 21:26:04 -0700

    Multiple bars

--- launch.sh
+++ launch.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 killall -q polybar
-polybar 2>&1 | tee -a /tmp/mybar.log & disown
+polybar 2>&1 left | tee -a /tmp/mybar.log & disown
+polybar 2>&1 middle | tee -a /tmp/mybar.log & disown
+polybar 2>&1 right | tee -a /tmp/mybar.log & disown
