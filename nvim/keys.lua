c2ef16c95f271c8f313b4324c0422028185cf428
mocha8686 <june.mocha8686@gmail.com>
2024-06-03 21:26:34 -0700

    fmt

--- nvim/keys.lua
+++ nvim/keys.lua
@@ -321,10 +321,35 @@ M.keys = {
 		{ '<leader>cc', '<Cmd>CccConvert<CR>',           desc = 'Convert color to other formats' },
 	},
 	['nvim-spectre'] = {
-		{ '<leader>ss', function() require('spectre').toggle() end, desc = 'Togggle Spectre'},
-		{ '<leader>sw', function() require('spectre').open_visual{select_word = true} end, desc = 'Search word/selection'},
-		{ '<leader>sw', function() require('spectre').open_visual() end, desc = 'Search word/selection', mode = { 'v' }},
-		{ '<leader>sp', function() require('spectre').open_file_search{select_word = true} end, desc = 'Search current file'},
+		{
+			'<leader>ss',
+			function()
+				require('spectre').toggle()
+			end,
+			desc = 'Togggle Spectre',
+		},
+		{
+			'<leader>sw',
+			function()
+				require('spectre').open_visual { select_word = true }
+			end,
+			desc = 'Search word/selection',
+		},
+		{
+			'<leader>sw',
+			function()
+				require('spectre').open_visual()
+			end,
+			desc = 'Search word/selection',
+			mode = { 'v' },
+		},
+		{
+			'<leader>sp',
+			function()
+				require('spectre').open_file_search { select_word = true }
+			end,
+			desc = 'Search current file',
+		},
 	},
 }
 
