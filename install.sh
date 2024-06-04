afeca1dcb64cd217b7c96693e0d4bde656939421
mocha8686 <june.mocha8686@gmail.com>
2024-06-03 21:26:04 -0700

    Add symlinks in install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
+
+###########
+# Configs #
+###########
 while read -r entry; do
 	fullsrc=$(echo "$entry" | cut -d ':' -f 1)
 	fulldest=$(echo "$entry" | cut -d ':' -f 2)
 
 	destdir=${fulldest/#\~/$HOME}
-	case $fullsrc in
-		*/*)
-			srcdir=$(echo "$fullsrc" | rev | cut -d '/' -f 2- | rev)
-			;;
-		*)
-			srcdir=.
-			;;
-	esac
-	srcname=$(echo "$fullsrc" | rev | cut -d '/' -f 1 | rev)
+	srcdir=$(dirname "$fullsrc")
+	srcname=$(basename "$fullsrc")
 
 	src=$(pwd)/$srcdir/$srcname
 	dest=$destdir
@@ -39,4 +36,27 @@ while read -r entry; do
 			ln -snf "$src" "$dest"
 			;;
 	esac
-done < config.txt
+done < configs.txt
+
+#########
+# Links #
+#########
+while read -r entry; do
+	target=$(echo "$entry" | cut -d ':' -f 1)
+	linkname=$(echo "$entry" | cut -d ':' -f 2)
+
+	target=${target/#\~/$HOME}
+	linkname=${linkname/#\~/$HOME}
+
+	targetdir=$(dirname "$target")
+	linknamedir=$(dirname "$linkname")
+
+	if [[ ! -d "$targetdir" ]]; then
+		mkdir -p "$targetdir"
+	fi
+	if [[ ! -d "$linknamedir" ]]; then
+		mkdir -p "$linknamedir"
+	fi
+
+	ln -snf "$target" "$linkname"
+done < links.txt
