0235f9c772fc0ec8d79d8c19de707e883685826a
mocha8686 <june.mocha8686@gmail.com>
2024-06-03 21:26:34 -0700

    wait for menu to disappear before screenshotting

--- rofi/screenshot_menu.sh
+++ rofi/screenshot_menu.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 op=$(echo -e "Screen capture\nSelect region\nScreen capture (clipboard)\nSelect region (clipboard)" | rofi -dmenu -no-show-icons -p "power")
 
+sleep 0.5
+
 case $op in
 	"Screen capture (clipboard)") grim - | wl-copy ;;
 	"Select region (clipboard)") grim -g "$(slurp)" - | wl-copy ;;
