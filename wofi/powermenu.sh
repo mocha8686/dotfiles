b0d7419b875d4b9b8a6ff6f33d28b514c6218eb8
mocha8686 <june.mocha8686@gmail.com>
2024-06-03 21:26:04 -0700

    Remove suspend

--- wofi/powermenu.sh
+++ wofi/powermenu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-op=$(echo -e " Poweroff\n Reboot\n Suspend\n Lock" | wofi -s ~/.config/wofi/wofi.css -i -S dmenu | awk '{print tolower($2)}')
+op=$(echo -e " Poweroff\n Reboot\n Lock" | wofi -s ~/.config/wofi/wofi.css -i -S dmenu | awk '{print tolower($2)}')
 
 case $op in
-	poweroff|reboot|suspend) systemctl "$op";;
+	poweroff|reboot) systemctl "$op";;
 	lock) swaylock;;
 esac
