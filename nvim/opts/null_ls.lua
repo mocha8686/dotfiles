7c1938aa7c79fb659d651139d200e177209f7d0f
mocha8686 <june.mocha8686@gmail.com>
2024-06-03 21:26:34 -0700

    remove prettier

--- nvim/opts/null_ls.lua
+++ nvim/opts/null_ls.lua
@@ -2,7 +2,7 @@ local null_ls = require 'null-ls'
 
 local opts = {
 	sources = {
-		null_ls.builtins.formatting.prettierd.with {
+		null_ls.builtins.diagnostics.stylelint.with {
 			extra_filetypes = { 'svelte' },
 		},
 		null_ls.builtins.formatting.stylelint.with {
