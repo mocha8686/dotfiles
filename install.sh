ba962b7dee57658afe4d8e32555fc8aa09551bf6
mocha8686 <june.mocha8686@gmail.com>
2024-06-03 21:26:04 -0700

    shellcheck

--- install.sh
+++ install.sh
@@ -1,45 +1,45 @@
 #!/bin/bash
-for entry in $(cat config.txt); do
-	fullsrc=$(echo $entry | cut -d ':' -f 1)
-	fulldest=$(echo $entry | cut -d ':' -f 2)
+while read -r entry; do
+	fullsrc=$(echo "$entry" | cut -d ':' -f 1)
+	fulldest=$(echo "$entry" | cut -d ':' -f 2)
 
 	destdir=${fulldest/#\~/$HOME}
 	case $fullsrc in
 		*/*)
-			srcdir=$(echo $fullsrc | rev | cut -d '/' -f 2- | rev)
+			srcdir=$(echo "$fullsrc" | rev | cut -d '/' -f 2- | rev)
 			;;
 		*)
 			srcdir=.
 			;;
 	esac
-	srcname=$(echo $fullsrc | rev | cut -d '/' -f 1 | rev)
+	srcname=$(echo "$fullsrc" | rev | cut -d '/' -f 1 | rev)
 
 	src=$(pwd)/$srcdir/$srcname
 	dest=$destdir
 
 	while true; do
 		if [[ ! -d $destdir ]]; then 
-			mkdir -p $destdir
+			mkdir -p "$destdir"
 		fi
 
 		if [[ -f $destdir/$srcname ]]; then
 			break
 		fi
 
-		read -p "Install $srcname to $destdir? [Y/n] " yn
+		read -rp "Install $srcname to $destdir? [Y/n] " yn
 		case $yn in
 			[Yy]*)
 
-				ln -sni $src $dest
+				ln -sni "$src" "$dest"
 				break
 				;;
 			[Nn]*)
 				break
 				;;
 			*)
-				ln -sni $src $dest
+				ln -sni "$src" "$dest"
 				break
 				;;
 		esac
 	done
-done
+done < config.txt
