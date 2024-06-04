9ba951b68bc16b4b4df03397cb6944ad9f287094
mocha8686 <june.mocha8686@gmail.com>
2024-06-03 21:26:34 -0700

    make power menu better

--- rofi/power_menu.sh
+++ rofi/power_menu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-op=$(echo -e " Poweroff\n Reboot\n Lock" | rofi -dmenu -no-show-icons -p "power" | awk '{print tolower($2)}')
+op=$(echo -e " Poweroff\n Reboot\n Lock" | rofi -dmenu -no-show-icons -i -p "power" | awk '{print tolower($2)}')
 
 case $op in
 	poweroff|reboot) systemctl "$op";;
-	lock) swaylock;;
+	lock) loginctl lock-session;;
 esac
