86337a33594001fe7f5a2169d8b114fd5fe13bda
mocha8686 <june.mocha8686@gmail.com>
2024-06-03 21:26:33 -0700

    fmt

--- nvim/opts/null_ls.lua
+++ nvim/opts/null_ls.lua
@@ -2,9 +2,12 @@ local null_ls = require 'null-ls'
 
 local opts = {
 	sources = {
-		null_ls.builtins.formatting.prettierd.with({
+		null_ls.builtins.formatting.prettierd.with {
 			extra_filetypes = { 'svelte' },
-		}),
+		},
+		null_ls.builtins.formatting.stylelint.with {
+			extra_filetypes = { 'svelte' },
+		},
 	},
 }
 
