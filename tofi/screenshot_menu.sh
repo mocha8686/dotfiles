d3c5cdf660bf9403f771e0365f972f4c5ce9d94a
mocha8686 <june.mocha8686@gmail.com>
2024-07-24 01:45:53 -0700

    add imv to screenshot menu

--- tofi/screenshot_menu.sh
+++ tofi/screenshot_menu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-op=$(echo -e "Screen capture\nSelect region\nScreen capture (clipboard)\nSelect region (clipboard)" | tofi --prompt-text "screenshot: ")
+op=$(echo -e "Select region (clipboard)\nScreen capture (clipboard)\nSelect region (imv)\nScreen capture (imv)\nSelect region\nScreen capture" | tofi --prompt-text "screenshot: ")
 
 sleep 0.5
 
@@ -8,4 +8,6 @@ case $op in
 	"Select region (clipboard)") grim -g "$(slurp)" - | wl-copy ;;
 	"Screen capture") grim ;;
 	"Select region") grim -g "$(slurp)" ;;
+	"Screen capture (imv)") grim /tmp/imv && imv /tmp/imv ;;
+	"Select region (imv)") grim -g "$(slurp)" /tmp/imv && imv /tmp/imv ;;
 esac
