b87e79f08c48b552cd6f39d716ce67c69ddff555
mocha8686 <june.mocha8686@gmail.com>
2024-12-11 22:44:55 -0800

    create separate files for imv

--- tofi/screenshot_menu.sh
+++ tofi/screenshot_menu.sh
@@ -4,10 +4,10 @@ op=$(echo -e "Select region (clipboard)\nScreen capture (clipboard)\nSelect regi
 sleep 0.5
 
 case $op in
-	"Screen capture (clipboard)") grim - | wl-copy ;;
-	"Select region (clipboard)") grim -g "$(slurp)" - | wl-copy ;;
-	"Screen capture") grim ;;
-	"Select region") grim -g "$(slurp)" ;;
-	"Screen capture (imv)") grim /tmp/imv && imv /tmp/imv ;;
-	"Select region (imv)") grim -g "$(slurp)" /tmp/imv && imv /tmp/imv ;;
+"Screen capture (clipboard)") grim - | wl-copy ;;
+"Select region (clipboard)") grim -g "$(slurp)" - | wl-copy ;;
+"Screen capture") grim ;;
+"Select region") grim -g "$(slurp)" ;;
+"Screen capture (imv)") dir=$(mktemp) && grim "$dir" && imv "$dir" ;;
+"Select region (imv)") dir=$(mktemp) && grim -g "$(slurp)" "$dir" && imv "$dir" ;;
 esac
