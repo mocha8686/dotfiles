6167cf1ca7a912bcf77ecdb641e2a7bccf72131c
mocha8686 <june.mocha8686@gmail.com>
2024-12-11 23:19:38 -0800

    fix hyprpaper

--- bin/change_wallpaper.sh
+++ bin/change_wallpaper.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-wal -i "$1"
+wallust run "$1"
 
 # hyprpaper
-if command -v hyprpaper > /dev/null; then
+if command -v hyprpaper >/dev/null; then
 	killall -q hyprpaper
-	hyprpaper > /dev/null 2>&1 & disown -h
+	hyprpaper >/dev/null 2>&1 &
+	disown -h
 fi
