3a1d5aac80ad3c957347b97d451c69242ca98e47
mocha8686 <june.mocha8686@gmail.com>
2025-11-06 09:58:40 -0800

    fix telescope fzf native build script

--- nvim/plugins/telescope/telescope-fzf-native.lua
+++ nvim/plugins/telescope/telescope-fzf-native.lua
@@ -1,5 +1,5 @@
 return {
 	'nvim-telescope/telescope-fzf-native.nvim',
 	build =
-	'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
+	'cmake -S. -Bbuild -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
 }
