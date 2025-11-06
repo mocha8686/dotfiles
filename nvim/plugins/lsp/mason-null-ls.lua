1e715230833517176ac1acf4a77b8af48667cc90
mocha8686 <june.mocha8686@gmail.com>
2025-11-06 09:59:42 -0800

    only activate stylelint/pretiterd if config exists

--- nvim/plugins/lsp/mason-null-ls.lua
+++ nvim/plugins/lsp/mason-null-ls.lua
@@ -1,7 +1,24 @@
+local function opts()
+	local mason_null_ls = require 'mason-null-ls'
+
+	return {
+		automatic_installation = true,
+		handlers = {
+			stylelint = function(source_name, methods)
+				if vim.fs.root(0, { 'stylelint.config.js' }) then
+					mason_null_ls.default_setup(source_name, methods)
+				end
+			end,
+			prettierd = function(source_name, methods)
+				if vim.fs.root(0, { '.prettierrc' }) then
+					mason_null_ls.default_setup(source_name, methods)
+				end
+			end,
+		},
+	}
+end
+
 return {
 	'jay-babu/mason-null-ls.nvim',
-	opts = {
-		automatic_installation = true,
-		handlers = {},
-	},
+	opts = opts,
 }
