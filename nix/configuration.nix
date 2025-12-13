f34f2c50f360e42389d39e27bc8ac3ebb2c94427
mocha8686 <june.mocha8686@gmail.com>
2025-12-13 12:16:29 -0800

    move home manager to here

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, ... } @ args:
 
 {
   imports =
@@ -58,9 +58,6 @@
     shell = pkgs.zsh;
   };
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
   fonts.packages = with pkgs; [
     pkgs.nerd-fonts.iosevka
   ];
@@ -70,7 +67,7 @@
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
-    neovim
+    (neovim.override { configure = { packages.myPlugins = { start = [ args.lazyNixHelper ]; }; }; })
     wget
     uwsm
     git
--- nix/flake.nix
+++ nix/flake.nix
@@ -3,13 +3,40 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    homeManager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    lazyNixHelper = {
+      url = "github:b-src/lazy-nix-helper.nvim";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, homeManager, ... } @ inputs:
+  let
+    pkgs = import nixpkgs { system = "x86_64-linux"; config.allowUnfree = true; };
+    lazyNixHelper = pkgs.vimUtils.buildVimPlugin {
+      name = "lazy-nix-helper";
+      src = inputs.lazyNixHelper;
+    };
+  in
+  {
     nixosConfigurations = {
       asahina = nixpkgs.lib.nixosSystem {
+        pkgs = pkgs;
+        specialArgs = { lazyNixHelper = lazyNixHelper; };
         system = "x86_64-linux";
-        modules = [ ./configuration.nix ];
+        modules = [
+          ./configuration.nix
+          homeManager.nixosModules.home-manager {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.mocha = ./home.nix;
+            };
+          }
+        ];
       };
     };
   };
