17e46a043bc4c2e832c8d5bced898d28ccbb43fb
mocha8686 <june.mocha8686@gmail.com>
2025-12-13 19:42:12 -0800

    customize statusbar

--- nix/programs/nixvim.nix
+++ nix/programs/nixvim.nix
@@ -1,8 +1,45 @@
-{ pkgs, inputs, ... }:
+{ pkgs, lib, inputs, ... }:
 {
   plugins = {
+    mini-diff.enable = true;
+    mini-git.enable = true;
+    mini-icons = {
+      enable = true;
+      mockDevIcons = true;
+    };
+
     mini-statusline = {
       enable = true;
+      settings = let
+        fn = lib.nixvim.utils.mkRaw ''
+function()
+  local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
+  local git           = MiniStatusline.section_git({ trunc_width = 40 })
+  local diff          = MiniStatusline.section_diff({ trunc_width = 75 })
+  local diagnostics   = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+  local lsp           = MiniStatusline.section_lsp({ trunc_width = 75 })
+  local filename      = MiniStatusline.section_filename({ trunc_width = 140 })
+  local fileinfo      = MiniStatusline.section_fileinfo({ trunc_width = 120 })
+  -- local location      = MiniStatusline.section_location({ trunc_width = 75 })
+  local location      = '%l %v'
+  local search        = MiniStatusline.section_searchcount({ trunc_width = 75 })
+
+  return MiniStatusline.combine_groups({
+    { hl = mode_hl,                  strings = { string.upper(mode) } },
+    { hl = 'MiniStatuslineDevinfo',  strings = { git, diff, diagnostics, lsp } },
+    '%<', -- Mark general truncate point
+    { hl = 'MiniStatuslineFilename', strings = { filename } },
+    '%=', -- End left alignment
+    { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+    { hl = mode_hl,                  strings = { search, location } },
+  })
+end
+'';
+      in
+      {
+        content.active = fn;
+        content.inactive = fn;
+      };
     };
   };
 
