5a1373a0aa7558aff00721503573b81191878255
mocha8686 <june.mocha8686@gmail.com>
2026-03-27 00:34:20 -0700

    darwin switch
    darwin switch

--- nix/programs/nixvim.nix
+++ nix/programs/nixvim.nix
@@ -332,12 +332,9 @@
 
     -- <CR> accepts first completion
     _G.cr_action = function()
-      -- If there is selected item in popup, accept it with <C-y>
-      if vim.fn.complete_info()['selected'] ~= -1 then return '\25' end
-      -- Fall back to plain `<CR>`. You might want to customize according
-      -- to other plugins. For example if 'mini.pairs' is set up, replace
-      -- next line with `return MiniPairs.cr()`
-      return '\r'
+      complete_info = vim.fn.complete_info()
+      if #complete_info['items'] > 0 and complete_info['selected'] == -1 then return '<C-n><C-y>' end
+      return MiniPairs.cr()
     end
     vim.keymap.set('i', '<CR>', 'v:lua.cr_action()', { expr = true })
 
